b24b408648586bd71d20f75a25df4fa4d1b5f256
TALHA HAROON <152915208+talha765haroon@users.noreply.github.com>
2024-02-25 03:14:39 +0500

    Add files via upload

---
+++ q5.ts
@@ -0,0 +1,8 @@
+/* Ques # 05:
+Famous Quote 2:Repeat exercise 4, but this time store the famous person name in a variable, called famous person . Then compose your message and store it in a new variable called message . print your  message.*/
+
+
+var famous_Person:string = "Albert Einstein";
+var message:string = "There are only two ways to live your life. One is as though nothing is a miracle.The other is as though everything is a miracle";
+console.log(`${famous_Person} once said,"${message}"`)
+
